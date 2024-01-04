32df1f26491c9bd4a5e3f80aa66562afd50cb760
Maximilian Merten <maximilian.merten@ewerk.com>
2024-01-04 10:55:02 +0100

    unsignificant change

--- src/app/views/case-history/case-history.component.ts
+++ src/app/views/case-history/case-history.component.ts
@@ -7,6 +7,8 @@ import {Observable} from "rxjs";
 import {CasesData} from "../../models/cases";
 import {CanvasJSAngularChartsModule} from "@canvasjs/angular-charts";
 import {convertISOToDayMonthYear} from "../../utils/utils";
+import {DeathsData} from "../../models/deaths";
+import {ContentWrapperComponent} from "../../shared-components/content-wrapper/content-wrapper.component";
 
 @Component({
   selector: 'app-case-history',
@@ -19,22 +21,32 @@ export class CaseHistoryComponent implements OnInit {
 
   private apiService: ApiGrabService = inject(ApiGrabService);
 
-  data: Array<any> = [];
+  casesData: Array<any> = [];
+  deathsData: Array<any> = [];
 
   casesData$: Observable<CasesData> = this.apiService.getCasesData();
+  deathsData$: Observable<DeathsData> = this.apiService.getDeaths()
 
   ngOnInit(): void {
     this.casesData$.subscribe(caseData => {
       caseData.data.forEach(entry => {
-        this.data.push({
+        this.casesData.push({
           label: convertISOToDayMonthYear(entry.date),
           y: entry.cases
         })
       })
     })
+    this.deathsData$.subscribe(deathsData => {
+      deathsData.data.forEach(entry => {
+        this.deathsData.push({
+          label: convertISOToDayMonthYear(entry.date),
+          y: entry.deaths
+        })
+      })
+    })
   }
 
-  chart = {
+  chartCases = {
     title: {
       text: "Cases History"
     },
@@ -43,7 +55,20 @@ export class CaseHistoryComponent implements OnInit {
     },
     data: [{
       type: "spline",
-      dataPoints: this.data
+      dataPoints: this.casesData
+    }]
+  };
+
+  chartDeaths = {
+    title: {
+      text: "Deaths History"
+    },
+    axisY: {
+      title: "Deaths",
+    },
+    data: [{
+      type: "spline",
+      dataPoints: this.deathsData
     }]
   };
 }
